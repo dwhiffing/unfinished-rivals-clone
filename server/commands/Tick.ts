b1921d11f59e7597378ffa1409392c30f512527f
Daniel Whiffing <danielwhiffing@gmail.com>
2020-12-05 14:38:52 -0500

    Fixes disconnect issue

--- server/commands/Tick.ts
+++ server/commands/Tick.ts
@@ -5,7 +5,11 @@ import { SyncCommand } from './Sync'
 export class TickCommand extends Command<RoomState> {
   execute() {
     if (this.state.phaseIndex === -1) return
-    if (this.state.phaseIndex === 1) return this.room.disconnect()
+    if (this.state.phaseIndex === 1) {
+      this.state.phaseIndex = 2
+      this.room.disconnect()
+      return []
+    }
     if (this.state.phaseIndex === 0) this.state.strategyGame.tick()
 
     return [new SyncCommand()]
